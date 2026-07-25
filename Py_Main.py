f26acb52b9543df811fa64ae14f24b103883df06
Ivan Turynsky <ivan.turinskyi.25@kse.org.ua>
2026-07-25 21:41:55 +0300

    The API use completely failed due to anti-bot restrictions of Yahoo and Stooq, after trying multiple times, it was decided to download csv file manually.

--- Py_Main.py
+++ Py_Main.py
@@ -1,4 +1,18 @@
-import yfinance as yf
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+
+csv_filename = "aapl_us_d.csv"
+
+df = pd.read_csv(csv_filename, parse_dates=['Date'], index_col='Date')
+
+df.columns = df.columns.str.capitalize()
+
+df = df.sort_index()
+
+df = df.loc['2022-01-01':'2026-01-01']
+
+# Retain required OHLCV columns and drop missing values
+df = df[['Open', 'High', 'Low', 'Close', 'Volume']].dropna()
+
+print(df)
